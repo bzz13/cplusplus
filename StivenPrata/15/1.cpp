fdfdc6ad3fe04c4cee9f930f1dfae3c21472fbd4
BZz13 <BZz13@skbkontur.ru>
2016-09-20 17:46:15 +0500

    bzz13: 1 by task

--- StivenPrata/15/1.cpp
+++ StivenPrata/15/1.cpp
@@ -9,6 +9,7 @@ class Tv
 {
 	friend class Remote;
 
+	bool isOn;
 	int channel;
 	int volume;
 	const int maxChannel = 20;
@@ -16,29 +17,41 @@ class Tv
 
 	void checkAnswer(const Remote& remote, const int & answer) const;
 public:
-	Tv(int c = 1, int v = 5): channel(c), volume(v) { };
+	Tv(int c = 1, int v = 5): isOn(false), channel(c), volume(v) { };
 
-	void nextChannel() { channel = channel == maxChannel ? channel + 1 : 1; }
-	void prevChannel() { channel = channel == 1 ? maxChannel : 1; }
+	void turnOn() { isOn = true; }
+	void turnOff() { isOn = false; }
 
-	void upVolume() { volume = volume == maxVolume ? maxVolume : volume + 1; }
-	void downVolume() { volume = volume == 0 ? 0 : volume - 1; }
+	void nextChannel() { if (isOn) channel = channel == maxChannel ? channel + 1 : 1; else throw logic_error("tv is off"); }
+	void prevChannel() { if (isOn) channel = channel == 1 ? maxChannel : 1; else throw logic_error("tv is off"); }
+
+	void upVolume() { if (isOn) volume = volume == maxVolume ? maxVolume : volume + 1; else throw logic_error("tv is off"); }
+	void downVolume() { if (isOn) volume = volume == 0 ? 0 : volume - 1; else throw logic_error("tv is off"); }
+
+	void switchRemoteInteractive(Remote& remote) const;
 
 	friend ostream & operator<<(ostream& os, const Tv & tv);
 };
 
 ostream & operator<<(ostream& os, const Tv & tv)
 {
-	return (os << "channel: " << tv.channel << " volume: " << tv.volume << endl);
+	if (tv.isOn)
+		return (os << "Tv is on. Now channel: " << tv.channel << " volume: " << tv.volume << endl);
+	else
+		return (os << "Tv is off" << endl);
 }
 
 class Remote
 {
 	friend class Tv;
 
+	bool isInteractive;
 	void vibrate() const { cout << "\tbzzzzz"; }
 public:
-	Remote() { };
+	Remote(): isInteractive(false) { };
+
+	void turnOn(Tv& tv) { tv.isOn = true; }
+	void turnOff(Tv& tv) { tv.isOn = false; }
 
 	void setChannel(Tv& tv, int c)
 	{
@@ -47,7 +60,6 @@ public:
 		else
 			throw invalid_argument("incorrect channel");
 	}
-
 	void nextChannel(Tv& tv) { tv.nextChannel(); }
 	void prevChannel(Tv& tv) { tv.prevChannel(); }
 
@@ -55,6 +67,8 @@ public:
 	void downChannel(Tv& tv) { tv.downVolume(); }
 
 	void setAnswer(const Tv& tv, const int & answer) { tv.checkAnswer(*this, answer); }
+
+	bool IsInteractive() const { return isInteractive; }
 };
 
 inline void Tv::checkAnswer(const Remote& remote, const int & answer) const
@@ -65,10 +79,18 @@ inline void Tv::checkAnswer(const Remote& remote, const int & answer) const
 		remote.vibrate();
 }
 
+inline void Tv::switchRemoteInteractive(Remote& remote) const
+{
+	remote.isInteractive = remote.isInteractive ? false : true;
+}
+
 int main()
 {
 	Tv tv;
 	Remote remote;
+	cout << tv << endl;
+
+	remote.turnOn(tv);
 	cout << tv << endl;
 
 	remote.setChannel(tv, 15);
@@ -83,6 +105,10 @@ int main()
 	catch(invalid_argument& ia)
 	{
 		cout << ia.what() << endl;
+	}
+	catch(logic_error& le)
+	{
+		cout << le.what() << endl;
 	}
 
 	for (int i = 0; i < 10; ++i)
