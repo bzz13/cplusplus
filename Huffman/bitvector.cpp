35df680d854a1e23cb2d5ab82e3fce863ee3312d
BZz13 <BZz13@skbkontur.ru>
2017-02-03 18:47:55 +0500

    bzz13: splitting to modules are completed

--- Huffman/bitvector.cpp
+++ Huffman/bitvector.cpp
@@ -1,25 +1,26 @@
 #include "bitvector.h"
 
+
 /* bitvector_reference impl*/
 
-bitvector_reference::bitvector_reference(bitvector* vector, unsigned int position):
+bitvector::bitvector_reference::bitvector_reference(bitvector* vector, size_t position):
     m_bitvector(vector), m_position(position)
 {
 }
 
-unsigned char bitvector_reference::get_bitvector_bit_mask(int bit_index) const
+unsigned char bitvector::bitvector_reference::get_bitvector_bit_mask(int bit_index) const
 {
-    if (bit_index > size_b)
+    if (bit_index > bitvector::size_b)
     {
-        throw std::out_of_range(std::to_string(bit_index) + " is too big. max value is " + std::to_string(size_b - 1));
+        throw std::out_of_range(std::to_string(bit_index) + " is too big. max value is " + std::to_string(bitvector::size_b - 1));
     }
-    return (unsigned char)(1 << (size_b - 1 - bit_index));
+    return (unsigned char)(1 << (bitvector::size_b - 1 - bit_index));
 }
 
-bitvector_reference& bitvector_reference::operator=(bool value)
+bitvector::bitvector_reference& bitvector::bitvector_reference::operator=(bool value)
 {
-    auto byte_index = m_position / size_b;
-    auto bit_index = m_position % size_b;
+    auto byte_index = m_position / bitvector::size_b;
+    auto bit_index = m_position % bitvector::size_b;
     unsigned char mask = get_bitvector_bit_mask(bit_index);
     unsigned char current = m_bitvector->values[byte_index] & mask;
 
@@ -37,18 +38,117 @@ bitvector_reference& bitvector_reference::operator=(bool value)
     return *this;
 }
 
-bitvector_reference::operator bool() const
+bitvector::bitvector_reference::operator bool() const
 {
-    auto byte_index = m_position / size_b;
-    auto bit_index = m_position % size_b;
+    auto byte_index = m_position / bitvector::size_b;
+    auto bit_index = m_position % bitvector::size_b;
     auto mask = get_bitvector_bit_mask(bit_index);
     return (bool)(m_bitvector->values[byte_index] & mask);
 }
 
-std::ostream& operator<<(std::ostream& os, const bitvector_reference& reference)
+std::ostream& operator<<(std::ostream& os, const bitvector::bitvector_reference& reference)
 {
     return (os << ((bool)reference));
 }
+
+
+
+
+
+
+
+/* bitvector_iterator impl*/
+
+bitvector::bitvector_iterator::bitvector_iterator(bitvector* bv, size_t pos)
+    : bitvector::bitvector_reference(bv, pos)
+{
+}
+
+bitvector::bitvector_iterator& bitvector::bitvector_iterator::operator++() //prefix increment
+{
+    m_position ++;
+    return (*this);
+}
+
+bitvector::bitvector_iterator bitvector::bitvector_iterator::operator++(int) //postfix increment
+{
+    auto tmp = *this;
+    m_position ++;
+    return bitvector_iterator(tmp);
+}
+
+bitvector::bitvector_reference& bitvector::bitvector_iterator::operator*()
+{
+    return *((bitvector::bitvector_reference*)this);
+}
+
+bool bitvector::bitvector_iterator::operator==(const bitvector_iterator& other) const
+{
+    return (m_bitvector == other.m_bitvector && m_position == other.m_position);
+}
+
+bool bitvector::bitvector_iterator::operator!=(const bitvector_iterator& other) const
+{
+    return (m_bitvector != other.m_bitvector || m_position != other.m_position);
+}
+
+bitvector::bitvector_iterator::operator bool() const
+{
+    return (bool)((bitvector::bitvector_reference)(*this));
+}
+
+
+
+
+
+
+
+
+/* const_bitvector_iterator impl*/
+
+
+bitvector::const_bitvector_iterator::const_bitvector_iterator(bitvector* bv, size_t pos)
+    : bitvector::bitvector_reference(bv, pos)
+{
+}
+// const_bitvector_iterator::const_bitvector_iterator(iterator& it): bitvector::bitvector_reference( it.m_bitvector, it.m_position) { }
+
+bitvector::const_bitvector_iterator& bitvector::const_bitvector_iterator::operator++() //prefix increment
+{
+    m_position ++;
+    return (*this);
+}
+
+bitvector::const_bitvector_iterator bitvector::const_bitvector_iterator::operator++(int) //postfix increment
+{
+    auto tmp = *this;
+    m_position ++;
+    return const_bitvector_iterator(tmp);
+}
+
+const bitvector::bitvector_reference& bitvector::const_bitvector_iterator::operator*() const
+{
+    return *((bitvector::bitvector_reference*)this);
+}
+
+bool bitvector::const_bitvector_iterator::operator==(const const_bitvector_iterator& other) const
+{
+    return (m_bitvector == other.m_bitvector && m_position == other.m_position);
+}
+
+bool bitvector::const_bitvector_iterator::operator!=(const const_bitvector_iterator& other) const
+{
+    return (m_bitvector != other.m_bitvector || m_position != other.m_position);
+}
+
+bitvector::const_bitvector_iterator::operator bool() const
+{
+    return (bool)((bitvector::bitvector_reference)(*this));
+}
+
+
+
+
 
 
 
@@ -57,12 +157,12 @@ bitvector::bitvector(): m_size(0)
 {
 }
 
-bitvector::bitvector(unsigned int initial_length, bool initial_value)
-    : m_size((initial_length / size_b) * size_b),
-      values(initial_length / size_b, initial_value ? (unsigned char)(~0) : (unsigned char)(0))
+bitvector::bitvector(size_t initial_length, bool initial_value)
+    : m_size((initial_length / bitvector::size_b) * bitvector::size_b),
+      values(initial_length / bitvector::size_b, initial_value ? (unsigned char)(~0) : (unsigned char)(0))
 {
 
-    for (int i = values.size() * size_b; i < initial_length; ++i)
+    for (int i = values.size() * bitvector::size_b; i < initial_length; ++i)
     {
         push_back(initial_value);
     }
@@ -80,7 +180,7 @@ bitvector::bitvector(const char* str): bitvector(std::string(str))
 {
 }
 
-unsigned int bitvector::size() const
+size_t bitvector::size() const
 {
     return m_size;
 }
@@ -94,13 +194,13 @@ void bitvector::clear()
 void bitvector::push_back(bool v)
 {
     m_size++;
-    auto bit_index = m_size % size_b;
+    auto bit_index = m_size % bitvector::size_b;
     if (bit_index == 1)
     {
         values.push_back(0);
     }
     auto tmp = const_cast<bitvector*>(this);
-    bitvector_reference ref(tmp, m_size - 1);
+    bitvector::bitvector_reference ref(tmp, m_size - 1);
     ref = v;
 }
 
@@ -112,23 +212,23 @@ void bitvector::push_back(const bitvector& other)
     }
 }
 
-const bitvector_reference bitvector::operator[](unsigned int pos) const
+const bitvector::bitvector_reference bitvector::operator[](size_t pos) const
 {
     if (pos >= m_size)
     {
         throw std::out_of_range(std::to_string(pos) + " is too big. size() is " + std::to_string(m_size));
     }
     auto tmp = const_cast<bitvector*>(this);
-    return bitvector_reference(tmp, pos);
+    return bitvector::bitvector_reference(tmp, pos);
 }
 
-bitvector_reference bitvector::operator[](unsigned int pos)
+bitvector::bitvector_reference bitvector::operator[](size_t pos)
 {
     if (pos >= m_size)
     {
         throw std::out_of_range(std::to_string(pos) + " is too big. size() is " + std::to_string(m_size));
     }
-    return bitvector_reference(this, pos);
+    return bitvector::bitvector_reference(this, pos);
 }
 
 std::ostream& operator<<(std::ostream& os, const bitvector& bv)
@@ -164,11 +264,11 @@ std::istream& operator>>(std::istream& is, bitvector& bv)
     else
     {
         bv.clear();
-        unsigned int size;
+        size_t size;
         is >> size;
         bv.m_size = size;
 
-        auto byte_size = size % size_b == 0 ? size / size_b : size / size_b + 1;
+        auto byte_size = size % bitvector::size_b == 0 ? size / bitvector::size_b : size / bitvector::size_b + 1;
         char c;
         is.get(c); // space
         for(auto i = 0; i < byte_size; ++i)
@@ -180,23 +280,23 @@ std::istream& operator>>(std::istream& is, bitvector& bv)
     return is;
 }
 
-bitvector_iterator bitvector::begin()
+bitvector::bitvector_iterator bitvector::begin()
 {
     return bitvector_iterator(this, 0);
 }
 
-bitvector_iterator bitvector::end()
+bitvector::bitvector_iterator bitvector::end()
 {
     return bitvector_iterator(this, m_size);
 }
 
-const_bitvector_iterator bitvector::begin() const
+bitvector::const_bitvector_iterator bitvector::begin() const
 {
     auto tmp = const_cast<bitvector*>(this);
     return const_bitvector_iterator(tmp, 0);
 }
 
-const_bitvector_iterator bitvector::end() const
+bitvector::const_bitvector_iterator bitvector::end() const
 {
     auto tmp = const_cast<bitvector*>(this);
     return const_bitvector_iterator(tmp, m_size);
--- Huffman/bitvector.h
+++ Huffman/bitvector.h
@@ -8,112 +8,61 @@
 #include <string>
 #include <memory>
 
-const int size_b = 8;
-
-class bitvector;
-
-struct bitvector_reference
-{
-    unsigned int m_position;
-    bitvector*   m_bitvector;
-
-    bitvector_reference(bitvector*, unsigned int);
-
-    unsigned char get_bitvector_bit_mask(int bit_index) const;
-
-    bitvector_reference& operator=(bool);
-
-    operator bool() const;
-
-    friend std::ostream& operator<<(std::ostream&, const bitvector_reference&);
-};
-
-struct bitvector_iterator: public bitvector_reference, public std::iterator<std::forward_iterator_tag, bool>
-{
-    bitvector_iterator(bitvector* bv, unsigned int pos = 0): bitvector_reference(bv, pos) { }
-    bitvector_iterator& operator++() //prefix increment
-    {
-        m_position ++;
-        return (*this);
-    }
-    bitvector_iterator operator++(int) //postfix increment
-    {
-        auto tmp = *this;
-        m_position ++;
-        return bitvector_iterator(tmp);
-    }
-    bitvector_reference& operator*()
-    {
-        return *((bitvector_reference*)this);
-    }
-    bool operator==(const bitvector_iterator& other) const
-    {
-        return (m_bitvector == other.m_bitvector && m_position == other.m_position);
-    }
-    bool operator!=(const bitvector_iterator& other) const
-    {
-        return (m_bitvector != other.m_bitvector || m_position != other.m_position);
-    }
-    operator bool() const
-    {
-        return (bool)((bitvector_reference)(*this));
-    }
-};
-
-
-struct const_bitvector_iterator: public bitvector_reference, public std::iterator<std::forward_iterator_tag, bool>
-{
-    const_bitvector_iterator(bitvector* bv, unsigned int pos = 0): bitvector_reference(bv, pos) { }
-    // const_bitvector_iterator(iterator& it): bitvector_reference( it.m_bitvector, it.m_position) { }
-
-    const_bitvector_iterator& operator++() //prefix increment
-    {
-        m_position ++;
-        return (*this);
-    }
-    const_bitvector_iterator operator++(int) //postfix increment
-    {
-        auto tmp = *this;
-        m_position ++;
-        return const_bitvector_iterator(tmp);
-    }
-    const bitvector_reference& operator*() const
-    {
-        return *((bitvector_reference*)this);
-    }
-    bool operator==(const const_bitvector_iterator& other) const
-    {
-        return (m_bitvector == other.m_bitvector && m_position == other.m_position);
-    }
-    bool operator!=(const const_bitvector_iterator& other) const
-    {
-        return (m_bitvector != other.m_bitvector || m_position != other.m_position);
-    }
-    operator bool() const
-    {
-        return (bool)((bitvector_reference)(*this));
-    }
-};
 
 class bitvector
 {
-    friend class bitvector_reference;
-
     std::vector<unsigned char> values;
-    unsigned int m_size;
+    size_t m_size;
 public:
+    static const int size_b = 8;
+
+    struct bitvector_reference
+    {
+        size_t      m_position;
+        bitvector*  m_bitvector;
+
+        bitvector_reference(bitvector*, size_t);
+        unsigned char get_bitvector_bit_mask(int bit_index) const;
+        bitvector_reference& operator=(bool);
+        operator bool() const;
+        friend std::ostream& operator<<(std::ostream&, const bitvector_reference&);
+    };
+
+    struct bitvector_iterator: public bitvector_reference, public std::iterator<std::forward_iterator_tag, bool>
+    {
+        bitvector_iterator(bitvector* bv, size_t pos = 0);
+        bitvector_iterator& operator++(); //prefix increment
+        bitvector_iterator operator++(int); //postfix increment
+        bitvector_reference& operator*();
+        bool operator==(const bitvector_iterator& other) const;
+        bool operator!=(const bitvector_iterator& other) const;
+        operator bool() const;
+    };
+
+    struct const_bitvector_iterator: public bitvector_reference, public std::iterator<std::forward_iterator_tag, bool>
+    {
+        const_bitvector_iterator(bitvector* bv, size_t pos = 0);
+        // const_bitvector_iterator(iterator& it);
+        const_bitvector_iterator& operator++(); //prefix increment
+        const_bitvector_iterator operator++(int); //postfix increment;
+        const bitvector_reference& operator*() const;
+        bool operator==(const const_bitvector_iterator& other) const;
+        bool operator!=(const const_bitvector_iterator& other) const;
+        operator bool() const;
+    };
+
     bitvector();
-    bitvector(unsigned int, bool initial_value = false);
+    bitvector(size_t, bool initial_value = false);
     bitvector(std::string);
     bitvector(const char*);
 
-    unsigned int size() const;
+    size_t size() const;
     void clear();
     void push_back(bool);
     void push_back(const bitvector&);
 
-    const bitvector_reference operator[](unsigned int) const;
-          bitvector_reference operator[](unsigned int);
+    const bitvector_reference operator[](size_t) const;
+          bitvector_reference operator[](size_t);
 
     friend std::ostream& operator<<(std::ostream&, const bitvector_reference&);
     friend std::ostream& operator<<(std::ostream&, const bitvector&);
